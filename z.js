c78737a50b996c18efc5336bcb774f3e0f60da47
StrickeX <32539745+StrickeX@users.noreply.github.com>
2018-01-11 12:36:58 +0100

    Update z.js

--- z.js
+++ z.js
@@ -1,10 +1,8 @@
 
-src="https://code.jquery.com/jquery-3.2.1.min.js";
 
 
 log = function(){
 document.getElementById("container-reg").style.display = "none";
-$('#main').addClass('blur');
 document.getElementById("container-log").style.display = "flex";
 }
 reg = function(){
